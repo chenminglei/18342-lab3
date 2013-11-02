50c0eda3429d71517c5356d4310669ab75f9e754
Minglei Chen <chenzhuokb2@ubuntu.(none)>
2013-11-01 17:40:07 -0700

    irq

--- kernel/timeSetup.c
+++ kernel/timeSetup.c
@@ -7,16 +7,14 @@
 
 void timeSetup() {
     reg_clear(INT_ICLR_ADDR, 1 << INT_OSTMR_0);
-    volatile unsigned long tmp1 = *(unsigned long *)0x40D00004;
-    reg_set(INT_ICMR_ADDR, 1 << INT_OSTMR_0);
-    volatile unsigned long tmp2 = *(unsigned long *)0x40D00004;
-    if (tmp1 != tmp2) {
-        printf("correct!");
-    }
+    //volatile unsigned long tmp1 = *(unsigned long *)0x40D00004;
+    reg_write(INT_ICMR_ADDR, 1 << INT_OSTMR_0);
+    //volatile unsigned long tmp2 = *(unsigned long *)0x40D00004;
+    //if (tmp1 != tmp2) {
+    //    printf("correct!");
+    // }
     reg_write(OSTMR_OSMR_ADDR(0), OSMR_COUNT);
     reg_write(OSTMR_OSCR_ADDR, 0);
     reg_set(OSTMR_OIER_ADDR, OSTMR_OIER_E0);
 }
-
-
 
