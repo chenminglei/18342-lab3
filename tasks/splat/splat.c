9e47b857baed31aad8f92bc3e47072b460ec45ff
zhe <zhe@zhe-VirtualBox.(none)>
2013-11-03 00:47:39 -0400

    comments for hello.c, typo.c and splat.c

--- tasks/splat/splat.c
+++ tasks/splat/splat.c
@@ -1,29 +1,29 @@
-/** @file splat.c
+/**
+ * splat.c: Display a spinning cursor
  *
- * @brief Displays a spinning cursor.
- *
- * Links to libc.
+ * Authors: Di Li <dil1@andrew.cmu.edu>
+ *          Zhe Shen <zshen@andrew.cmu.edu>
+ *          Minglei Chen <mingleic@andrew.cmu.edu>
+ * Date:    00:37 AM  Nov 3rd 2013
  */
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
-int main(int argc, char** argv)
-{
-	
-	/* Add code here -- put your names at the top. */
-	while(1) {
-	    printf("|");
-	    sleep(200);
-            printf("\b");
-	    printf("/");
-	    sleep(200);
-            printf("\b");
-	    printf("-");
-	    sleep(200);
-            printf("\b");
-	    printf("\\");
-	    sleep(200);
-            printf("\b");
+int main(int argc, char** argv) {
+	while (1) {
+		printf("|");
+		sleep(200);
+		printf("\b");
+		printf("/");
+		sleep(200);
+		printf("\b");
+		printf("-");
+		sleep(200);
+		printf("\b");
+		printf("\\");
+		sleep(200);
+		printf("\b");
 	}
 	return 0;
 }
