3522d5cc22fe1d5f97904a93873da1c958064c36
Minglei Chen <chenzhuokb2@ubuntu.(none)>
2013-11-02 21:50:53 -0700

    comment by minglei

--- kernel/installHandler.c
+++ kernel/installHandler.c
@@ -1,15 +1,30 @@
+/*                                                                              
+ * installHandler.c: hijack the swi or irq handler                                      
+ *                                                                              
+ * Authors: Di Li <dil1@andrew.cmu.edu>                                      
+ *          Zhe Shen <zshen@andrew.cmu.edu>
+ *          Minglei Chen <mingleic@andrew.cmu.edu>                                      
+ * Date:    7:47 PM  Nov 3rd 2013                                             
+ */ 
+
 #include <exports.h>
 #include "main.h"
 #include "constant.h"
 
 
+/* the address of the swi handler*/
 extern unsigned int *jump_swi;
+/* the address of the irq handler*/
 extern unsigned int *jump_irq;
 
+/* the original first instruction of the swi handler*/
 extern unsigned int swi_instr1;
+/* the original second instruction of the swi handler*/
 extern unsigned int swi_instr2;
 
+/* the original first instruction of the irq handler*/
 extern unsigned int irq_instr1;
+/* the original second instruction of the irq handler*/
 extern unsigned int irq_instr2;
 
 
@@ -28,11 +43,14 @@ void installHandler(unsigned int * vec_address, unsigned int new_address, unsign
         jump_handler = (unsigned int *)((0x00000fff & vec_instr) + (unsigned int)vec_address + 0x08);
         jump_handler = (unsigned int *)(*jump_handler);
 
-        if (type == 0) {
+
+        /* if it is the swi handler */
+        if (type == HANDLER_SWI) {
             jump_swi = jump_handler;
             swi_instr1 = *jump_swi;
             swi_instr2 = *(jump_swi + 1);
         }
+        /* if it is the irq handler */
         else {
             jump_irq = jump_handler;
             irq_instr1 = *jump_irq;
--- kernel/swi_sleep.c
+++ kernel/swi_sleep.c
@@ -1,3 +1,11 @@
+/*                                                                              
+ * swi_sleep.c: handling sleep of swi interrupt                                       
+ *                                                                              
+ * Authors: Di Li <dil1@andrew.cmu.edu>                                      
+ *          Zhe Shen <zshen@andrew.cmu.edu>
+ *          Minglei Chen <mingleic@andrew.cmu.edu>                                      
+ * Date:    7:47 PM  Nov 3rd 2013                                             
+ */ 
 
 #include "constant.h"
 #include <arm/timer.h>
@@ -5,11 +13,16 @@
 
 extern volatile unsigned int cur_time;
 extern volatile unsigned int cur_bias;
+
 void swi_sleep(unsigned *reg) {
+    /* Get the time requested by the user program */
     unsigned long millis = (unsigned long)reg[0];
+    /* Get current time */
     unsigned long currenttime = cur_time;
+    /* Get the cumulated bias for oscr */
     unsigned long bias = cur_bias / OSMR_ONE_SECOND;
 
+    /* Break until sleep enough */
     while (currenttime * TIME_RESOLUTION + millis + bias > cur_time * TIME_RESOLUTION + cur_bias / OSMR_ONE_SECOND) {
     };
 }
